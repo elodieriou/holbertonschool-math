66b38405884e49d0d6d0fec381b2f7fce2cc6c5d
Elodie Riou <elodie.riou@hotmail.com>
2021-11-05 16:33:31 +0100

    Add file 0-display.c

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * display_complex_number - function that displays the complex numbers
+ *
+ * @c: the complex number
+ *
+ * Return: Nothing
+ */
+void display_complex_number(complex c)
+{
+	if (c.re != 0 && c.im)
+		printf("%.0f + %.0fi\n", c.re, c.im);
+	else if (c.re == 0)
+		printf("%.0fi\n", c.im);
+	else
+		printf("%.0f\n", c.re);
+}
