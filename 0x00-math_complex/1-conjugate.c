aa9a6163e84b283ce7c034f3b7c4e44563a1f770
Elodie Riou <elodie.riou@hotmail.com>
2021-11-07 17:50:48 +0100

    Add file 1-conjugate.c

---
+++ 0x00-math_complex/1-conjugate.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * conjugate - function that returns the conjugate of a given complex number
+ *
+ * @c: a complex number
+ *
+ * Return: a conjugate of a complex number
+ */
+complex conjugate(complex c)
+{
+	c.im = -1 * c.im;
+	c.re = c.re;
+	return (c);
+}
