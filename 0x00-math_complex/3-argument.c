13c0a7b8e4f6e07e0d1fc4b8024df32fa1b848fc
Elodie Riou <elodie.riou@hotmail.com>
2021-11-11 13:13:29 +0100

    Fix code

--- 0x00-math_complex/3-argument.c
+++ 0x00-math_complex/3-argument.c
@@ -3,15 +3,10 @@
 
 /**
  * argument - function that returns the argument of a given complex number
- *
  * @c: a complex number
- *
  * Return: the argument of a complex number
  */
 double argument(complex c)
 {
-	double args;
-
-	args = atan(c.im / c.re);
-	return (args);
+	return (atan(c.im / c.re));
 }
