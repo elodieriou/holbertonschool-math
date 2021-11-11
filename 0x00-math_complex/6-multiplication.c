dff9a7b5ce7d6357bae265093a83035d8dc15977
Elodie Riou <elodie.riou@hotmail.com>
2021-11-11 16:53:56 +0100

    Add file 6-multiplication.c

---
+++ 0x00-math_complex/6-multiplication.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * multiplication - function that performs the multiplication operation to complex numbers
+ * @c1: a number
+ * @c2: another number
+ * @c3: a pointer to the struct complex
+ * Return: Nothing
+ */
+void multiplication(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re * c2.re - c1.im * c2.im;
+	c3->im = c1.im * c2.re + c1.re * c2.im;
+}
