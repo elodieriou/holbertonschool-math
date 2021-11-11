5e64b359be8440467b817bef2ab438eb1e3272e0
Elodie Riou <elodie.riou@hotmail.com>
2021-11-11 12:19:05 +0100

    fix code with added condition

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -14,6 +14,10 @@ void display_complex_number(complex c)
 		printf("%.0fi\n", c.im);
 	else if (c.im == -1)
 		printf("%.0f - i\n", c.re);
+	else if (c.im == 1)
+		printf(".0f + i\n", c.re);
+	else if (c.im < 0)
+		printf("%.0f - %.0fi\n", c.re, c.im * -1);
 	else
 		printf("%.0f + %.0fi\n", c.re, c.im);
 }
