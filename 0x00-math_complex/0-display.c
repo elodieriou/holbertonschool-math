ce4377ed8cebfa648767ac47e953ccbadc4a7057
Elodie Riou <elodie.riou@hotmail.com>
2021-11-11 12:16:14 +0100

    fix code with added condition

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -3,17 +3,17 @@
 
 /**
  * display_complex_number - function that displays the complex numbers
- *
  * @c: the complex numbers
- *
  * Return: Nothing
  */
 void display_complex_number(complex c)
 {
-	if (c.re != 0 && c.im)
-		printf("%.0f + %.0fi\n", c.re, c.im);
+	if (c.im == 0)
+		printf("%.0f", c.re);
 	else if (c.re == 0)
 		printf("%.0fi\n", c.im);
+	else if (c.im == -1)
+		printf("%.0f - i\n", c.re);
 	else
-		printf("%.0f\n", c.re);
+		printf("%.0f + %.0fi\n", c.re, c.im);
 }
